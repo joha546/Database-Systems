8412bd3889204b8466e45d46914602007c8e9725
Md Khaled Bin Joha <joh60483@gmail.com>
2024-11-24 23:02:17 +0600

    Weather Station 4

---
+++ HackerRank/SQL Basics/Revising the Select Query II.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+-- github.com/joha546
+
+SELECT NAME FROM CITY 
+WHERE COUNTRYCODE='USA' and POPULATION > 120000;
---
+++ HackerRank/SQL Basics/Select By ID.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+-- github.com/joha546
+
+SELECT * FROM CITY 
+WHERE ID = '1661';
